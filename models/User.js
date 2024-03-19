75a468f1546ad1a7f0acf4fed8ac9125ab44debf
KunalKapure05 <kapurekunal61@gmail.com>
2024-03-20 02:08:50 +0530

    Updated

--- models/User.js
+++ models/User.js
@@ -34,38 +34,9 @@ const userSchema = new mongoose.Schema({
 })  
 
 
-userSchema.pre('save',async function(req,res){
-    const user = this;
-
-    if(!user.isModified('password')) return next();
-
-    try {
-
-        const salt = bcrypt.genSalt(10);
-        const hashedPassword = await bcrypt.hash(user.password, salt);
-
-        user.password = hashedPassword;
-        next();
-
-        
-    } catch (error) {
-        return next(err);
-        
-    }
 
 
-})
 
-userSchema.methods.comparePassword = async function(candidatePassword){
-    try {
-        
-        const isMatch = await bcrypt.compare(candidatePassword,this.password);
-        return isMatch;
-
-    } catch (error) {
-        throw err;
-    }
-}
 
 
 const User = mongoose.model('User', userSchema);
